c14a845576a2481f5e5d38ff5c4899d1c73a08fb
BARRAUD Laurent <Laurent.BARRAUD@cpnv.ch>
2019-04-09 12:00:30 +0200

    Ajouté plusieurs tuiles dessinées à l'écran

--- AnimSprites/frmAnimSprites.cs
+++ AnimSprites/frmAnimSprites.cs
@@ -22,13 +22,13 @@ namespace AnimSprites
             // Crée un objet représentant l'image que l'on veut afficher.
             // Cet objet doit être un membre de la classe qui hérite de Image,
             // comme un Bitmap ou Metafile (WMF).
-            Bitmap bmp = new Bitmap(@"D:\doc\Cpnv\MA-24-appui-programmation\AnimSprites\AnimSprites\Resources\nature-tileset.jpg");
+            Bitmap bmp = new Bitmap(AnimSprites.Properties.Resources.nature_tileset);
 
-            // Crée un rectangle pour afficher l'image.
-            Rectangle destRect = new Rectangle(100, 100, 100, 100);
+            // Crée un rectangle pour afficher l'image, avec comme paramètres la coordonnée x et y du coin supérieur-gauche, la largeur et la hauteur.
+            Rectangle destRect = new Rectangle(0, 0, 50, 50);
 
             // Crée un rectangle pour la source de l'image.
-            Rectangle srcRect = new Rectangle(50, 50, 50, 50);
+            Rectangle srcRect = new Rectangle(34, 2, 31, 31);
 
             // Crée un objet graphique qui représente la surface de dessin de notre bitmap.   
             Graphics g = Graphics.FromImage(bmp);
@@ -37,10 +37,16 @@ namespace AnimSprites
             GraphicsUnit units = GraphicsUnit.Pixel;
 
             picPlateforme2.Image = bmp;
+            picPlateforme3.Image = bmp;
+            picPlateforme4.Image = bmp;
+            picPlateforme5.Image = bmp;
 
-            // Appelle la fonction DrawImage de l'objet graphique pour rendre l'image à l'écran.
+            // Appelle la fonction DrawImage de l'objet graphique pour rendre les images à l'écran.
             // On doit spécifier à la fois l'image et les coordonnées où elle va être dessinée.
             g.DrawImage(picPlateforme2.Image, destRect, srcRect, units);
+            g.DrawImage(picPlateforme3.Image, destRect, srcRect, units);
+            g.DrawImage(picPlateforme4.Image, destRect, srcRect, units);
+            g.DrawImage(picPlateforme5.Image, destRect, srcRect, units);
 
             // On doit ensuite disposer de l'objet graphique.
             g.Dispose();
