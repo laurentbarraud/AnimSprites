b4f31eccde16b5a62b8e27bdbfa83bdb6b5d561f
laurentbarraud <lau.barraud@gmail.com>
2025-07-02 22:28:18 +0200

    a) Ajouté une méthode utilitaire d’extraction de tuile dans une classe TileHelper. b) Ajouté une interface IBreakable dans son propre fichier IBreakable.cs. c) Ajouté une classe de type BreakableSolidPictureBox qui hérite de SolidPictureBox et implémente IBreakable.

---
+++ AnimSprites/BreakableSolidPictureBox.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ComponentModel;
+
+namespace AnimSprites
+{
+    public class BreakableSolidPictureBox : SolidPictureBox, IBreakable
+    {
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public int Health { get; protected set; } = 1;
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public bool IsDestroyed { get; protected set; } = false;
+
+        public BreakableSolidPictureBox(Image tileset, int tileX, int tileY, int tileSize)
+        {
+            Width = tileSize;
+            Height = tileSize;
+            BackgroundImage = TileHelper.GetTileImage(tileset, tileX, tileY, tileSize, tileSize);
+            BackgroundImageLayout = ImageLayout.Stretch;
+            BackColor = Color.Transparent;
+        }
+
+        public virtual void Hit()
+        {
+            if (IsDestroyed) return;
+
+            Health--;        
+
+            if (Health <= 0)
+            {
+                IsDestroyed = true;               
+            }
+        }
+    }
+
+}
---
+++ AnimSprites/IBreakable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimSprites
+{
+    public interface IBreakable
+    {
+        void Hit();                // Called when the object is hit
+        bool IsDestroyed { get; } // Returns true if already destroyed
+    }
+}
+
